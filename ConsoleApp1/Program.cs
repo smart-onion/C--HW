81751152a8a3004ad9a4d34adfa9bc52cfb16916
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-06-01 16:57:57 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int stairs = Convert.ToInt32(Console.ReadLine());
+        int width = stairs * 2;
+        int height = stairs * 2;
+
+        int stepNext = 0;
+
+        for (int y = 0; y < width; y++) 
+        { 
+            for(int x = 0; x < height; x++) 
+            {
+                if (y % 2 == 0 && x == stepNext)
+                {
+                    Console.Write("***");
+                    
+                }
+                if (x == stepNext && y % 2 != 0)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write(" ");
+            }
+
+            if (y % 2 == 0)
+            {
+                stepNext += 2;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
