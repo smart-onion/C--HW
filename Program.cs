338c46ef625b5e0ce5b81b7c78c7ae2d4038124f
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-07-08 14:58:35 +0300

    xml not working

--- Program.cs
+++ Program.cs
@@ -1,53 +1,38 @@
 ﻿
+using System;
 using System.Collections;
-using System.Reflection;
-using System.Text.RegularExpressions;
-public class Program
- {
-    public static void Main()
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+using static Program;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
     {
-        DirectoryInfo root = new DirectoryInfo("G:/Games");
-        FileInfo[] files = root.GetFiles("*", SearchOption.AllDirectories);
+        Student student = new Student("student1", "st1");
+        Student studen2 = new Student("student2", "st2");
+        Student studen3 = new Student("student3", "st3");
+        Student studen4 = new Student("student4", "st4");
+        Student studen5 = new Student("student5", "st5");
+        Student studen6 = new Student("student6", "st6");
+        Group group = new Group();
 
-        Dictionary<string, long[]> dict = new Dictionary<string, long[]>();
+        group.AddStudent(student);
+        group.AddStudent(studen6);
+        group.AddStudent(studen2);
+        group.AddStudent(studen3);
+        group.AddStudent(studen4);
+        group.AddStudent(studen5);
 
-        long totalSize = 0;
-        long totalCount = 0;
+        group.ShowGroup();
 
-        foreach (FileInfo item in files)
-        {
-            if (dict.ContainsKey(item.Extension))
-            {
-                dict[item.Extension][0]++;
-                dict[item.Extension][1] += item.Length;
-            }
-            else
-            {
-                if (item.Extension.Equals("")) continue;
+        var s = new XmlSerializer(typeof(Group));
 
-                dict.Add(item.Extension, new long[2] { 1, item.Length });
-            }
-            totalSize += item.Length;
-            totalCount++;
-        }
+        TextWriter sw = new StreamWriter("./file.xml");
 
-        int number = 1;
-        Console.WriteLine("+----+---------------------+---------+---------------------+------------------+------------------+");
-        Console.WriteLine("| No | Extension           | Count   | Total Size in Bytes | % of total files | % of total bytes |");
-        Console.WriteLine("+----+---------------------+---------+---------------------+------------------+------------------+");
-
-        foreach (var item in dict.OrderBy(v => v.Value[0]).Reverse().Take(50))
-        {
-            double percentOfCount = (item.Value[0] / (float)totalCount) * 100;
-            double percentOfSize = (item.Value[1] / (float)totalSize) * 100;
-
-            Console.WriteLine("| {0,-2} | {1,-19} | {2,-7} | {3,-19} | {4,-16} | {5,-16} |", number, item.Key, item.Value[0], item.Value[1], Math.Round(percentOfCount, 2), Math.Round(percentOfSize, 2));
-            number++;
-        }
-
-        Console.WriteLine("+----+---------------------+---------+---------------------+------------------+------------------+");
-        Console.WriteLine("| TOTAL:                   | {0,-7} | {1,-19} | {2, -16} | {3, -16} |", totalCount, totalSize, 100, 100);
-        Console.WriteLine("+--------------------------+---------+---------------------+------------------+------------------+");
-
+        s.Serialize(sw, group);
+        sw.Close();
     }
 }
