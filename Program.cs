d18b17dec221d3f5aac2dab1520c56843cdbd66c
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-07-08 13:48:54 +0300

    Files / directories

--- Program.cs
+++ Program.cs
@@ -6,39 +6,48 @@ public class Program
  {
     public static void Main()
     {
-        StreamReader sr = new StreamReader("./Kobzar.txt");
-        string source = sr.ReadToEnd();
-        Regex reg = new Regex(@"\w{3,20}");
+        DirectoryInfo root = new DirectoryInfo("G:/Games");
+        FileInfo[] files = root.GetFiles("*", SearchOption.AllDirectories);
 
-        SortedList<string, int> dict = new SortedList<string, int>();
+        Dictionary<string, long[]> dict = new Dictionary<string, long[]>();
 
-        foreach(Match match in reg.Matches(source))
+        long totalSize = 0;
+        long totalCount = 0;
+
+        foreach (FileInfo item in files)
         {
-            string stringMatch = match.ToString().ToLower();
-
-
-            if (dict.ContainsKey(stringMatch))
+            if (dict.ContainsKey(item.Extension))
             {
-                dict[stringMatch]++;
+                dict[item.Extension][0]++;
+                dict[item.Extension][1] += item.Length;
             }
             else
             {
-                dict.Add(stringMatch, 1);
+                if (item.Extension.Equals("")) continue;
+
+                dict.Add(item.Extension, new long[2] { 1, item.Length });
             }
+            totalSize += item.Length;
+            totalCount++;
         }
-        var sortedByCount = dict.OrderBy(v => v.Value).Reverse();
 
         int number = 1;
-        Console.WriteLine("+----+----------------------+-------+");
-        Console.WriteLine("| No | Word                 | Count |");
-        Console.WriteLine("+----+----------------------+-------+");
+        Console.WriteLine("+----+---------------------+---------+---------------------+------------------+------------------+");
+        Console.WriteLine("| No | Extension           | Count   | Total Size in Bytes | % of total files | % of total bytes |");
+        Console.WriteLine("+----+---------------------+---------+---------------------+------------------+------------------+");
 
-        foreach (var item in sortedByCount.Take(50))
+        foreach (var item in dict.OrderBy(v => v.Value[0]).Reverse().Take(50))
         {
-            Console.WriteLine("| {0,-2} | {1,-20} | {2,5} |", number, item.Key, item.Value);
+            double percentOfCount = (item.Value[0] / (float)totalCount) * 100;
+            double percentOfSize = (item.Value[1] / (float)totalSize) * 100;
+
+            Console.WriteLine("| {0,-2} | {1,-19} | {2,-7} | {3,-19} | {4,-16} | {5,-16} |", number, item.Key, item.Value[0], item.Value[1], Math.Round(percentOfCount, 2), Math.Round(percentOfSize, 2));
             number++;
         }
-        Console.WriteLine("+----+----------------------+-------+");
+
+        Console.WriteLine("+----+---------------------+---------+---------------------+------------------+------------------+");
+        Console.WriteLine("| TOTAL:                   | {0,-7} | {1,-19} | {2, -16} | {3, -16} |", totalCount, totalSize, 100, 100);
+        Console.WriteLine("+--------------------------+---------+---------------------+------------------+------------------+");
 
     }
 }
