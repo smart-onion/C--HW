2c8aab9cf7de12117e0fe5304156442553eb57e6
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-10-07 15:50:23 +0300

    hw5

--- Program.cs
+++ Program.cs
@@ -1,84 +1,134 @@
-﻿using HW4;
-
-internal class Program
+﻿internal class Program
 {
-    public static event Func<bool> OnEvent;
-    static object locker = new(); 
+    delegate int MyDelegate(int[] arr, int start, int end);
+
     private static void Main(string[] args)
     {
-        // task 1
-        //OnEvent += () => true;
-        //BankTask();
-        // task 2
-        MovingWords fwd = new MovingWords(true, "fwd");
-        MovingWords back = new MovingWords(false, "back");
-        Thread fwdThread = new Thread(fwd.Move);
-        Thread backThread = new Thread(back.Move);
-        fwdThread.Start();
-        backThread.Start();
+        //LoadURLTask();
+        HW3();
     }
 
-    private static void BankTask()
+    static void LoadURLTask()
     {
-
-        Thread[] clients = new Thread[5];
-
-        for (int i = 0; i < clients.Length; i++)
+        List<string> urls = new List<string>
         {
-            clients[i] = new Thread(new Client().ReceiveMoney);
-            clients[i].Start();
-        }
-        Thread.Sleep(20000);
-        OnEvent += () => false;
-    }
+           "https://google.com",
+           "https://apple.com",
+           "https://telegram.org"
+        };
 
-    class Bank
-    {
-        protected static int bankAmount = 1000;
-        protected bool DestributeCash(int amount)
+        CancellationTokenSource cts = new();
+        cts.CancelAfter(TimeSpan.FromSeconds(5));
+        CancellationToken tocken = cts.Token;
+
+        Task t = Task.Run(() =>
         {
-            if (amount <= bankAmount)
+            while(true)
             {
-                bankAmount -= amount;
-                return true;
-            }
-            return false;
-        }
-    }
-
-    class ATM : Bank
-    {
-        public int GetCash(int amount)
-        {
-            if (DestributeCash(amount))
-            {
-                Console.Write($"Clent {Thread.CurrentThread.ManagedThreadId} received {amount}$\t Bank balance: {bankAmount} ");
-                return amount;
-            }
-            Console.Write($"Client {Thread.CurrentThread.ManagedThreadId}. Opp... money exeed bank balance {bankAmount} ");
-            return 0;
-        }
-    }
-
-    class Client
-    {
-        public int balance;
-        Random rand = new Random();
-        ATM atm = new ATM();
-        public void ReceiveMoney()
-        {
-            while (OnEvent.Invoke())
-            {
-                lock (locker)
+                tocken.ThrowIfCancellationRequested();
+                if (Console.ReadKey().KeyChar == 'c')
                 {
-                    int time = rand.Next(1000, 2000);
-                    Thread.Sleep(time);
-                    balance += atm.GetCash(time / 10);
-                    Console.WriteLine("\t\tClient Balance: " + balance);
+                    cts.Cancel();
                 }
             }
+            
+        }, tocken);
+
+        Parallel.ForEach(urls, async url =>
+        {
+           await LoadURL(url, tocken);
+        });
+
+        try
+        {
+            t.Wait();
+
         }
+        catch (Exception)
+        {
+
+            Console.WriteLine("Task wa canceled");
+        }
+
+    }
+    static async Task LoadURL(string url, CancellationToken token)
+    {
+        using HttpClient client = new HttpClient();
+
+        List<Task> downloadTasks = new List<Task>();
+
+
+        await Task.Run(async () =>
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url, token);
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Downloaded content from {url}");
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine($"Download from {url} was cancelled.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error downloading from {url}: {ex.Message}");
+            }
+        }, token);
+        
+
+        await Task.WhenAll(downloadTasks);
     }
 
+    static void HW2()
+    {
+        int[] array = new int[100];
+
+        int taskNumber = 4;
+        Task<int>[] tasks = new Task<int>[taskNumber];
+
+        int chunkSize = array.Length / taskNumber;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i;
+        }
+
+        MyDelegate func = (int[] arr, int start, int end) =>
+        {
+            int sum = 0;
+            for (int i = start; i < end - 1; i++) sum += arr[i];
+            return sum;
+        };
+
+
+        for (int i = 0; i < taskNumber; i++)
+        {
+            int start = i * chunkSize;
+            int end = start + chunkSize;
+            tasks[i] = Task.Run(() => func(array, start, end));
+        }
+
+
+        Task.WaitAll(tasks);
+        int result = tasks.Sum(t => t.Result);
+        Console.WriteLine("Sum = " + result);
+        Console.ReadLine();
+        
+    }
+
+    static void HW3()
+    {
+        int[] a = [4,2,3,2,12,3,4,234,123,32,4,123,323,4,1,1,1,2,3,2,2,1,2,3,2,1,4,5,4,3,2,34,];
+        int searcIndex = 0;
+        Task removeSame = Task<int>.Run(() => {  a = a.ToHashSet().ToArray(); });
+        Task sort = removeSame.ContinueWith(t => 
+        { a = a.Order().ToArray(); });
+        Task<int> search = sort.ContinueWith(t => { return a.ToList().BinarySearch(2); });
+
+        removeSame.Wait();
+        Console.WriteLine("Resulr = " + search.Result);
+
+    }
 
 }
