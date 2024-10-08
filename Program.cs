adb4449e948ac9889a70fdfcdc7a531bb7b29a55
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-10-08 14:07:49 +0300

    hw6

--- Program.cs
+++ Program.cs
@@ -1,134 +1,72 @@
 ﻿internal class Program
 {
-    delegate int MyDelegate(int[] arr, int start, int end);
-
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        //LoadURLTask();
-        HW3();
+        // Task 1
+        //string text = await ReadFromFileAsync("");
+        //Console.WriteLine(text.Length);
+
+        // Task 2
+        //var pathToDownload = "https://fs1.itstep.org/api/v1/files/mp0Kr-U5o3Wi7YpmPH0bDkgvPAtQ452W?inline=true";
+        //DonwlaodFileAsync(pathToDownload, "test2.docx");
+        //Console.ReadKey();
+
+        // Task 3
+        GetPrimeNumbers(100);
+        Console.WriteLine("Simulate another job");
+        Console.ReadKey();
     }
 
-    static void LoadURLTask()
+    static async Task<string> ReadFromFileAsync(string path)
     {
-        List<string> urls = new List<string>
-        {
-           "https://google.com",
-           "https://apple.com",
-           "https://telegram.org"
-        };
+        return await File.ReadAllTextAsync(path);
+    }
 
-        CancellationTokenSource cts = new();
-        cts.CancelAfter(TimeSpan.FromSeconds(5));
-        CancellationToken tocken = cts.Token;
+    static async void DonwlaodFileAsync(string pathToFile, string destionationPath)
+    {
+        using var client = new HttpClient();
+        var file = await client.GetAsync(pathToFile);
 
-        Task t = Task.Run(() =>
+
+        FileStream fileStream = new FileStream(destionationPath, FileMode.Create, FileAccess.Write);
+        await file.Content.CopyToAsync(fileStream );
+        Console.WriteLine("File downloaded successfully!");
+    }
+
+    static async void GetPrimeNumbers(int maxNumber)
+    {
+        var arr = await GeneratePrimeNumberAsync(maxNumber);
+    
+        foreach (var number in arr)
         {
-            while(true)
+            Console.Write(number + " ");
+        }
+    }
+
+    static async Task<List<int>> GeneratePrimeNumberAsync(int maxNumber)
+    {
+        return await Task.Run(() =>
+        {
+            List<int> primeNumbers = new();
+
+            for (int i = 2; i <= maxNumber; i++)
             {
-                tocken.ThrowIfCancellationRequested();
-                if (Console.ReadKey().KeyChar == 'c')
+                bool isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
                 {
-                    cts.Cancel();
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primeNumbers.Add(i);
                 }
             }
-            
-        }, tocken);
 
-        Parallel.ForEach(urls, async url =>
-        {
-           await LoadURL(url, tocken);
+            return primeNumbers;
         });
-
-        try
-        {
-            t.Wait();
-
-        }
-        catch (Exception)
-        {
-
-            Console.WriteLine("Task wa canceled");
-        }
-
     }
-    static async Task LoadURL(string url, CancellationToken token)
-    {
-        using HttpClient client = new HttpClient();
-
-        List<Task> downloadTasks = new List<Task>();
-
-
-        await Task.Run(async () =>
-        {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(url, token);
-                string content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Downloaded content from {url}");
-            }
-            catch (OperationCanceledException)
-            {
-                Console.WriteLine($"Download from {url} was cancelled.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error downloading from {url}: {ex.Message}");
-            }
-        }, token);
-        
-
-        await Task.WhenAll(downloadTasks);
-    }
-
-    static void HW2()
-    {
-        int[] array = new int[100];
-
-        int taskNumber = 4;
-        Task<int>[] tasks = new Task<int>[taskNumber];
-
-        int chunkSize = array.Length / taskNumber;
-
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = i;
-        }
-
-        MyDelegate func = (int[] arr, int start, int end) =>
-        {
-            int sum = 0;
-            for (int i = start; i < end - 1; i++) sum += arr[i];
-            return sum;
-        };
-
-
-        for (int i = 0; i < taskNumber; i++)
-        {
-            int start = i * chunkSize;
-            int end = start + chunkSize;
-            tasks[i] = Task.Run(() => func(array, start, end));
-        }
-
-
-        Task.WaitAll(tasks);
-        int result = tasks.Sum(t => t.Result);
-        Console.WriteLine("Sum = " + result);
-        Console.ReadLine();
-        
-    }
-
-    static void HW3()
-    {
-        int[] a = [4,2,3,2,12,3,4,234,123,32,4,123,323,4,1,1,1,2,3,2,2,1,2,3,2,1,4,5,4,3,2,34,];
-        int searcIndex = 0;
-        Task removeSame = Task<int>.Run(() => {  a = a.ToHashSet().ToArray(); });
-        Task sort = removeSame.ContinueWith(t => 
-        { a = a.Order().ToArray(); });
-        Task<int> search = sort.ContinueWith(t => { return a.ToList().BinarySearch(2); });
-
-        removeSame.Wait();
-        Console.WriteLine("Resulr = " + search.Result);
-
-    }
-
 }
