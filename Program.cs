6eca0f5af39b7637c7378736a8ac4b273fd5869c
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-11-07 08:46:53 +0200

    hw1

--- Program.cs
+++ Program.cs
@@ -1,28 +1,56 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using hw1;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var users = new List<User>();
+
 app.UseStaticFiles();
 
-
-app.Run(async (context) =>
+app.Run(async context =>
 {
     context.Response.ContentType = "text/html; charset=utf-8";
-    var path = context.Request.Path.ToString();
-    var response = context.Response;
-    var request = context.Request;
-    var part = Path.GetFileName(path);
+    var path = context.Request.Path;
 
-    if (request.Path == $"/api/length/{part}")
-    {
-        await response.WriteAsync($"Length = {part.Length}");
-    }
-
+	switch (path)
+	{
+		case "/":
+            await context.Response.SendFileAsync("wwwroot/index.html");
+            break;
+		case "/fillup":
+            await context.Response.SendFileAsync("wwwroot/fillup.html");
+            break;
+		case "/api/postuser":
+            if (context.Request.Method == "POST")
+            {
+                var form = context.Request.Form;
+                string name = form["name"];
+                string email = form["email"];
+                string phone = form["phone"];
+                users.Add(new User() { Name = name, Email = email, PhoneNumber = phone });
+                context.Response.Redirect("/greeting");
+            }
+            break;
+        case "/api/greeting":
+            if (context.Request.Method == "POST")
+            {
+                var form = context.Request.Form;
+                string name = form["name"];
+                await context.Response.WriteAsync($"<p>Hello {name}</p>");
+            }
+            else context.Response.Redirect("/greeting");
+            break;
+        case "/api/customers":
+            await context.Response.WriteAsJsonAsync(users);
+            break;
+        case "/greeting":
+            await context.Response.SendFileAsync("wwwroot/greet.html");
+            break;
+		default:
+			break;
+	}
 });
+
 app.UseDeveloperExceptionPage();
 
-
 app.Run();
-
