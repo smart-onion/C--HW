155e044d00352dbeb5cbdee36f625a79077e6d0d
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-11-15 19:09:35 +0200

    l23

---
+++ Middleware/NumberInterptitatorMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Humanizer;
+
+namespace Lesson2.Middleware
+{
+    public class NumberInterpretationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public NumberInterpretationMiddleware(RequestDelegate next) { _next = next; }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (int.TryParse(context.Request.Query["number"], out int number))
+            {
+                if (number > 1 && number <= 100000)
+                {
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync($"Your number {number.ToWords()}");
+                }
+                else context.Response.StatusCode = 400;
+            }
+            else context.Response.StatusCode = 402;
+
+            //await _next(context);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,70 +6,7 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(CheckMethods);
-
-app.MapWhen(
-    context => context.Request.Method == HttpMethods.Get,
-    appbuilder =>
-    {
-        appbuilder.Use(IfGetOnly);
-    });
-
-
-app.Use(IfPostAndJson);
-app.Use(IsPersonObj);
-
-app.Run(async context => await context.Response.WriteAsync("Hi"));
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseMiddleware<NumberInterpretationMiddleware>();
 app.Run();
-
-static async Task CheckMethods(HttpContext context, RequestDelegate next)
-{
-    if (context.Request.Method == HttpMethods.Get || context.Request.Method == HttpMethods.Post)
-    {
-        await next.Invoke(context);
-    }
-    else
-    {
-        context.Response.StatusCode = 404;
-        await context.Response.WriteAsync($"{context.Request.Method} restricted");
-    }
-}
-
-static async Task IfGetOnly(HttpContext context, RequestDelegate next)
-{
-    if (context.Request.Method == HttpMethods.Get)
-    {
-        context.Response.StatusCode = 200;
-        await context.Response.WriteAsync("Welcome to Person API!");
-    }
-}
-
-static async Task IfPostAndJson(HttpContext context, RequestDelegate next)
-{
-    if (context.Request.ContentType == "application/json") await next.Invoke(context);
-    else
-    {
-        context.Response.StatusCode = 401;
-        await context.Response.WriteAsync($"{context.Request.ContentType} restricted");
-    }
-}
-
-static async Task IsPersonObj(HttpContext context, RequestDelegate next)
-{
-    string jsonString;
-    using (var reader = new StreamReader(context.Request.Body))
-    {
-        jsonString = await reader.ReadToEndAsync();
-    }
-    var person = JsonSerializer.Deserialize<Person>(jsonString);
-    if (person == null)
-    {
-        context.Response.StatusCode = 401;
-        await context.Response.WriteAsync($"{jsonString} bad format");
-        return;
-    }
-    Console.WriteLine($"Person name: {person.Name}, Age: {person.Age}");
-    await next.Invoke(context);
-}
-record Person(string Name, int Age);
 
