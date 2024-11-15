fd585cf215204f7c5f72afba119109d488c1e90a
smart-onion <154732603+smart-onion@users.noreply.github.com>
2024-11-15 19:20:41 +0200

    l12

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseMiddleware<NumberInterpretationMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDirectoryBrowser();
+    app.UseDeveloperExceptionPage();
+    app.Run(async context => await context.Response.WriteAsync("You on Develepment stage"));
+
+}
+else if (app.Environment.IsStaging())
+{
+    app.UseDirectoryBrowser();
+    app.Run(async context => await context.Response.WriteAsync("You on Staging stage.Some tools can be blocked"));
+}
+
+else if (!app.Environment.IsProduction())
+{
+    app.Run(async context =>
+    {
+        Console.WriteLine($"{DateTime.Now} path: {context.Request.Path}");
+        if (context.Request.Path == "/wwwroot")
+        {
+            await context.Response.WriteAsync("Not allowed on production");
+        }
+    });
+}
+
 app.Run();
+
 
