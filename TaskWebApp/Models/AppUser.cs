6b5ae1405c41f6f1e8f8cdcdd9a5188d5f16cb4d
smart-onion <sanyamart13@gmail.com>
2026-06-13 12:04:29 +0300

    init

---
+++ TaskWebApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskWebApp.Models
+{
+    public class AppUser
+    {
+        public int      Id               { get; set; }
+        public string   Nickname         { get; set; } = string.Empty;
+        public string   Status           { get; set; } = string.Empty;
+        public DateTime BirthDate        { get; set; }
+        public DateTime RegisteredDate   { get; set; }
+        public DateTime LastLoginDate    { get; set; }
+        public string   AvatarUrl        { get; set; } = string.Empty;
+    }
+}
